a64b37f2a4890ecfa5ae77008ca660e45566037f
Tristan Rimmer <Tristan Rimmer>
2026-09-16 10:12:43 +0100

    Vulkan Wrapper: Added Texture Image Container

--- vulkan_wrapper/image/image_utils.hh
+++ vulkan_wrapper/image/image_utils.hh
@@ -6,12 +6,23 @@
 #include <utility>
 #include <vulkan/vulkan_raii.hpp>
 namespace ImageUtils {
-auto create_image(vk::raii::Device &device, vk::raii::PhysicalDevice &physical,
+auto create_image(const vk::raii::Device &device,
+                  const vk::raii::PhysicalDevice &physical,
                   std::pair<uint32_t, uint32_t> image_dimensions,
                   vk::Format image_format, vk::ImageTiling tiling,
                   vk::ImageUsageFlags usage, vk::MemoryPropertyFlags properties)
     -> std::expected<std::pair<vk::raii::Image, vk::raii::DeviceMemory>,
                      std::string>;
-}
+
+auto transition_image_layout(vk::raii::CommandBuffer &command_buffer,
+                             const vk::raii::Image &image,
+                             vk::ImageLayout old_layout,
+                             vk::ImageLayout new_layout) -> void;
+
+auto copy_buffer_to_image(vk::raii::CommandBuffer &command_buffer,
+                          const vk::raii::Buffer &buffer,
+                          vk::raii::Image &image, uint32_t width,
+                          uint32_t height) -> void;
+} // namespace ImageUtils
 
 #endif
